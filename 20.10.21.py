97887bcb3ecb462a180ca19a26f4846b09e7e0ff
Ruslan <vafin_2005@list.ru>
2021-10-20 19:06:13 +0300

    20.10.21.py wer

--- 20.10.21.py
+++ 20.10.21.py
@@ -1,12 +1,28 @@
+#class Zebra:
+
+  #  def which_stripe(self):
+   #  stripe = {1:'полоска белая',2: 'полоска черная'}
+
+ #   for key in stripe:
+  #       print(key)
+
+
+
+#z1=Zebra()
+#z1.which_stripe()
+
+
 class Zebra:
-
+    counter = 0
     def which_stripe(self):
-     stripe = {1:'полоска белая' ,2: 'полоска черная'}
-     for key in stripe:
-         print(stripe[1])
-         print(stripe[2])
+        if self.counter % 2 == 0:
+            print("Полоска белая")
+            Zebra.counter += 1
+        else:
+            print("Полоска черная")
+            Zebra.counter += 1
 
-
-z1=Zebra()
-z1.which_stripe()
+z1 = Zebra()
+z1.which_stripe() # печатает "Полоска белая"
+z1.which_stripe() # печатает "Полоска черная"
 z1.which_stripe()
