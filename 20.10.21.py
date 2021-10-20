fcec2bbc2d6c71e36167d8ad58de4e2278b30990
Ruslan <vafin_2005@list.ru>
2021-10-20 18:18:19 +0300

    20.10.21.py wer

---
+++ 20.10.21.py
@@ -0,0 +1,12 @@
+class Zebra:
+
+    def which_stripe(self):
+     stripe = {1:'полоска белая' ,2: 'полоска черная'}
+     for key in stripe:
+         print(stripe[1])
+         print(stripe[2])
+
+
+z1=Zebra()
+z1.which_stripe()
+z1.which_stripe()
