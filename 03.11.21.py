389edfd733b0977176d8185c721771b5b0422ebc
Ruslan <vafin_2005@list.ru>
2021-11-10 15:37:01 +0300

    27.10.21

--- 03.11.21.py
+++ 03.11.21.py
@@ -3,30 +3,17 @@ class Person:
         self.name=name
         self.surname=surname
         self.gender=gender
-
-
-
-
-
-
-
+        if self.gender != 'Male'and self.gender!='Female':
+            self.gender='Male'
+            print('Я не распознал пол.Пусть это будет мальчик  ')
     def __str__(self):
         if self.gender=='Male':
          return f'Гражданин {self.surname+" "+self.name}'
         if self.gender=='Female':
           return  f'Гражданка {self.surname + " " + self.name}'
-
-        elif self.gender!='Male'or'Female':
-            self.gender = 'Male'
-            print( 'Пусть будет мальчик ')
-            return f'Гражданин {self.surname + " " + self.name}'
-
-
-
-
 p1=Person('CHUCK','Norris')
 print(p1)
-p2=Person('Mila','Asia','Female')
+p2=Person('Asia','Dalis','Female')
 print(p2)
-p3=Person('David','Abas',True)
+p3=Person('David','Abas','dddd')
 print(p3)
